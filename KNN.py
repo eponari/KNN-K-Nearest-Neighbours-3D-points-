7d5bd17ed09b1dd57b61ce8c8bfa5d6d36cb49c2
Edison Ponari <eponari19@epoka.edu.al>
2020-08-24 08:16:32 +0200

    Fixed confusion about input

--- KNN.py
+++ KNN.py
@@ -2,18 +2,19 @@
 import operator
 from classes import *
 
-
+print("Enter the number of points and K value:")
 points,k=[int(x) for x in input().split()]
 
 items=[]
-
+print("For each point enter X,Y,Z seperated by space and press Enter:")
 for i in range(points):
   x,y,z,name=[x for x in input().split()]
   items.append(item(point(int(x),int(y),int(z)),name))
 
+print("Enter the number of points you want to classify:")
 requestedKNN=input()
 requestedKNN=int(requestedKNN)
-
+print("For each point enter X,Y,Z and the color of the point seperated by space and press Enter:")
 for i in range(requestedKNN):
   x,y,z=[int(x) for x in input().split()]
   main_point=point(x,y,z)
@@ -26,4 +27,4 @@ for i in range(requestedKNN):
     else:
       close_items[items[i].name]+=1
     i+=1
-  print(max(close_items.items(), key=operator.itemgetter(1,0))[0])
+  print("This points is classifed as",max(close_items.items(), key=operator.itemgetter(1,0))[0],".")
